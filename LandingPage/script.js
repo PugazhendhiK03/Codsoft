b38115b7703f4aa08bfa0977c296741a07e5b4a6
PugazhendhiK03 <pugazhenddhikrishna03@gmail.com>
2024-07-18 19:39:01 +0530

    level 1 task  finished

--- LandingPage/script.js
+++ LandingPage/script.js
@@ -2,3 +2,13 @@
 function cta() {
     alert('Sign up functionality coming soon!');
 }
+
+// Example: Add a smooth scroll effect for feature section links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
